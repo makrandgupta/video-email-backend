1362db7ea2e07087f803f8641f72608f81c7ce6b
Makrand Gupta <makrand1996@gmail.com>
2016-09-06 05:20:36 -0400

    endpoint updates

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
-    app.use('/', require('./routes/index'));
-    app.use('/api/video', require('./routes/api/video'));
+    app.use('/', require('./routes/public'));
+    app.use('/view', require('./routes/public/view'));
+
     app.use('/api/email', require('./routes/api/email'));
 
     app.use('/auth/google', require('./routes/auth/google'));
